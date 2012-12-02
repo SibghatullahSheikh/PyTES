4f5d1e0b03604a17bccbe9d82929b3b4dd64f85f
SAKAI, Kazuhiro <sakai@astro.isas.jaxa.jp>
2012-12-03 01:53:56 +0900

    Fixed white noise level

--- src/pytes/Pulse.py
+++ src/pytes/Pulse.py
@@ -6,17 +6,37 @@ def dummy(pha=1.0, tr=2e-6, tf=100e-6, points=1024, t=2e-3, duty=0.5, talign=0.0
     Generate Dummy Pulse
     """
     
-    ts = np.linspace(-t*duty, t*(1-duty), points) + t/points*talign
+    points = np.asarray(points)
     
+    # Data counts
+    c = 1 if points.ndim == 0 else np.ones(points[0])[np.newaxis].T
+    
+    # Data length
+    l = points if points.ndim == 0 else points[-1]
+    
+    # Time steps
+    ts = np.linspace(-t*duty, t*(1-duty), l)*c + t/l*np.asarray(talign)[np.newaxis].T
+    
+    # Pulse model function
     def M(t):
         return 0.0 if t < 0 else (tr+tf)/tf**2 * (1-np.exp(-t/tr)) * np.exp(-t/tf)
+    
+    # Vectorize pulse model function
     Mvec = np.vectorize(M)
     
-    return pha*tf*Mvec(ts)
+    return np.asarray(pha)[np.newaxis].T*tf*Mvec(ts)
 
 def white(var=3e-6**2, mean=0.0, t=2e-3, points=1024):
     """
     Generate Gaussian White Noise
     """
     
-    return (mean + np.sqrt(var)*norm.rvs(size=points)) / np.sqrt(t/(points[0] if type(points) in (list, tuple, np.array) else points))
+    points = np.asarray(points)
+    
+    # Data length
+    l = points if points.ndim == 0 else points[-1]
+    
+    # Time resolution of Nyquist frequency
+    dt = t / l * 2
+
+    return (mean + np.sqrt(var)*norm.rvs(size=points)) / np.sqrt(dt)
