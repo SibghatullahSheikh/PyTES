1ed80b6878d751874c0080f8c51711a952098a34
SAKAI, Kazuhiro <sakai@astro.isas.jaxa.jp>
2012-12-06 01:00:05 +0900

    Moved pulse (previously dummy) and white from Pulse to Simulate

--- src/pytes/Pulse.py
+++ src/pytes/Pulse.py
@@ -1,9 +1,23 @@
 import numpy as np
 from scipy.stats import norm
 
-def dummy(pha=1.0, tr=2e-6, tf=100e-6, points=1024, t=2e-3, duty=0.5, talign=0.0):
+def pulse(pha=1.0, tr=2e-6, tf=100e-6, points=1024, t=2e-3, duty=0.5, talign=0.0):
     """
     Generate Dummy Pulse
+    
+    Parameters (and their default values):
+        pha:    pulse height (Default: 1.0)
+        tr:     rise time constant (Default: 2 us)
+        tf:     fall time constant (Default: 100 us)
+        points: data length (scalar or 2-dim tuple/list) (Default: 1024)
+        t:      sample time (Default: 2 ms)
+        duty:   pulse raise time ratio to t (Default: 0.5)
+                should take from 0.0 to 1.0
+        talign: time offset ratio to dt (=t/points) (Default: 0)
+                should take from -0.5 to 0.5
+    
+    Return (pulse)
+        pulse:  pulse data (array-like)
     """
     
     points = np.asarray(points)
@@ -19,16 +33,26 @@ def dummy(pha=1.0, tr=2e-6, tf=100e-6, points=1024, t=2e-3, duty=0.5, talign=0.0
     
     # Pulse model function
     def M(t):
-        return 0.0 if t < 0 else (tr+tf)/tf**2 * (1-np.exp(-t/tr)) * np.exp(-t/tf)
+        return 0.0 if t < 0 else (1-np.exp(-t/tr)) * np.exp(-t/tf)
     
     # Vectorize pulse model function
     Mvec = np.vectorize(M)
     
-    return np.asarray(pha)[np.newaxis].T*tf*Mvec(ts)
+    # Normalize coeff (= max M)
+    norm = M(tr*np.log(tf/tr+1))
+    
+    return np.asarray(pha)[np.newaxis].T*Mvec(ts)/norm
 
-def white(var=3e-6**2, mean=0.0, t=2e-3, points=1024):
+def white(sigma=3e-6, mean=0.0, points=1024, t=2e-3):
     """
     Generate Gaussian White Noise
+    
+    Parameters (and their default values):
+        sigma:  noise level in V/srHz (or gaussian sigma) (Default: 3 uV/srHz)
+        mean:   DC offset of noise signal (Default: 0 V)
+        t:      sample time (Default: 2 ms)
+        points: data length (scalar or 2-dim tuple/list) (Default: 1024)
+        points: 
     """
     
     points = np.asarray(points)
@@ -39,4 +63,4 @@ def white(var=3e-6**2, mean=0.0, t=2e-3, points=1024):
     # Time resolution of Nyquist frequency
     dt = t / l * 2
 
-    return (mean + np.sqrt(var)*norm.rvs(size=points)) / np.sqrt(dt)
+    return (mean + sigma*norm.rvs(size=points)) / np.sqrt(dt)
