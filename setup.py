251b46efc71089c9eb7d5913cc0eea4daea5381d
SAKAI, Kazuhiro <sakai@astro.isas.jaxa.jp>
2012-12-19 00:55:19 +0900

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ from distutils.core import setup
 
 setup(
     name='PyTES',
-    version='0.1',
+    version='1.0-alpha',
     description='Python TES Utilities',
     author='Kazuhiro Sakai',
     author_email='sakai@astro.isas.jaxa.jp',
     url='https://github.com/robios/PyTES/',
     packages=['pytes'],
-    package_dir={'pytes': 'src/pytes'},
 )
